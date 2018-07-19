33b05d946f5fc498fe5867bb00947ac6cc6a7067
Etothepowerof26 <refertrionjared1@gmail.com>
2018-07-19 19:48:21 -0400

    Cleaning code up wave cl_hud_iteminfo.lua

--- gamemode/core/clientfiles/cl_hud_iteminfo.lua
+++ gamemode/core/clientfiles/cl_hud_iteminfo.lua
@@ -1,16 +1,12 @@
 local function DrawItemInfo() -- TODO: remake, easy job
 	for _, ent in pairs(ents.FindByClass("prop_*")) do
-		--if IsValid(ent) then
-			--if ent:GetModel() == "models/props_c17/oildrum001.mdl" then
-			--end
-		--end
 		if IsValid(ent) and ent:GetNWString("PrintName") and ent:GetNWInt("Amount") > 0 then
 			if ent:GetPos():Distance(LocalPlayer():GetPos()) < 200 then
-				local posENTpos = (ent:GetPos() + Vector(0, 0, 10)):ToScreen()
-				local strText = ent:GetNWString("PrintName")
-				if ent:GetNWInt("Amount") > 1 then strText = strText .. " x" .. ent:GetNWInt("Amount") end
+				local EntPos = (ent:GetPos() + Vector(0, 0, 10)):ToScreen()
+				local Text = ent:GetNWString("PrintName")
+				if ent:GetNWInt("Amount") > 1 then Text = Text .. " x" .. ent:GetNWInt("Amount") end
 				if ent:GetOwner() == LocalPlayer() or not IsValid(ent:GetOwner()) or LocalPlayer():IsInSquad(ent:GetOwner()) then
-					draw.SimpleTextOutlined(strText, "UiBold", posENTpos.x, posENTpos.y - 10, clrWhite, 1, 1, 1, clrDrakGray)
+					draw.SimpleTextOutlined(Text, "UiBold", EntPos.x, EntPos.y - 10, White, 1, 1, 1, DrakGray)
 					ent:SetColor(Color(255, 255, 255, 255))
 				else
 					ent:SetColor(Color(255, 255, 255, 0))
@@ -18,8 +14,8 @@ local function DrawItemInfo() -- TODO: remake, easy job
 			end
 			if ent:GetModel() == "models/props/de_inferno/clayoven.mdl" then
 				if ent:GetPos():Distance(LocalPlayer():GetPos()) < 200 then
-					local posENTpos = (ent:GetPos() + Vector(0, 0, 40)):ToScreen()
-					draw.SimpleTextOutlined("Work Bench", "UiBold", posENTpos.x, posENTpos.y, clrWhite, 1, 1, 1, clrDrakGray)
+					local EntPos = (ent:GetPos() + Vector(0, 0, 40)):ToScreen()
+					draw.SimpleTextOutlined("Work Bench", "UiBold", EntPos.x, EntPos.y, White, 1, 1, 1, DrakGray)
 				end
 			end
 		end
