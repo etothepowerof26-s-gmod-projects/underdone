8084afe48d1fdbbdd2b42ff1805025a932f237c8
Etothepowerof26 <refertrionjared1@gmail.com>
2018-07-20 10:42:30 -0400

    Well, first attempt at changing networking

--- gamemode/core/sharedfiles/sh_notification.lua
+++ gamemode/core/sharedfiles/sh_notification.lua
@@ -1,36 +1,41 @@
 if CLIENT then
-	local tblNotifications = {}
-	local intLastTime = 15
-	local intStartPosition = ScrH() - 100
-	local intSpacing = 5
-	local intHieght = 20
+	local Notifications = {}
+	local Delay = 15
+	local StartPosition = ScrH() - 100
+	local Spacing = 5
+	local Height = 20
 
 	local function DrawNotifications()
-		local yOffset = intStartPosition
-		for _, strNocification in pairs(tblNotifications) do
+		local yOffset = StartPosition
+		for _, Notification in pairs(Notifications) do
 			surface.SetFont("MenuLarge")
-			local wide, high = surface.GetTextSize(strNocification)
-			local pnlNotification = jdraw.NewPanel()
-			pnlNotification:SetDimensions(ScrW() - (wide + 40), yOffset, wide + 30, intHieght)
-			pnlNotification:SetStyle(4, clrTan)
-			pnlNotification:SetBorder(1, clrDrakGray)
-			jdraw.DrawPanel(pnlNotification)
-			draw.SimpleText(strNocification, "MenuLarge", pnlNotification.Position.X + intHieght, pnlNotification.Position.Y + 3, clrDrakGray, 0, 3)
-			yOffset = yOffset - intHieght - intSpacing
+			local wide, high = surface.GetTextSize(Notification)
+			local NotifPanel = jdraw.NewPanel()
+			NotifPanel:SetDimensions(ScrW() - (wide + 40), yOffset, wide + 30, Height)
+			NotifPanel:SetStyle(4, clrTan)
+			NotifPanel:SetBorder(1, clrDrakGray)
+			jdraw.DrawPanel(NotifPanel)
+			draw.SimpleText(Notification, "MenuLarge", NotifPanel.Position.X + Height, NotifPanel.Position.Y + 3, clrDrakGray, 0, 3)
+			yOffset = yOffset - Height - Spacing
 		end
 	end
 	hook.Add("HUDPaint", "DrawNotifications", DrawNotifications)
-
-	function AddNotification(strNotification)
-		table.insert(tblNotifications, 1, strNotification)
-		timer.Simple(intLastTime, function() table.remove(tblNotifications) end)
-	end
-	concommand.Add("UD_AddNotification", function(ply, command, args) AddNotification(table.concat(args, " ")) end)
+	
+	net.Receive("UD_AddNotification", function()
+		local Message = net.ReadString()
+		
+		table.insert(Notifications, 1, Message)
+		timer.Simple(Delay, function() table.remove(Notifications) end)
+	end)
 end
 
 if SERVER then
+	util.AddNetworkString("UD_AddNotification")
+	
 	local Player = FindMetaTable("Player")
-	function Player:CreateNotification(strMessage)
-		self:ConCommand("UD_AddNotification " .. strMessage)
+	function Player:CreateNotification(Message)
+		net.Start"UD_AddNotification"
+			net.WriteString(Message)
+		net.Send(self)
 	end
 end
