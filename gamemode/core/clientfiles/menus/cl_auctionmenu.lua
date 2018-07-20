7be8f735df5c88b1c802e3b039615beac19c2d2c
Etothepowerof26 <refertrionjared1@gmail.com>
2018-07-19 20:04:29 -0400

    AHhhahha more cleanup

--- gamemode/core/clientfiles/menus/cl_auctionmenu.lua
+++ gamemode/core/clientfiles/menus/cl_auctionmenu.lua
@@ -10,16 +10,16 @@ function PANEL:Init()
 	self.PlayerAuctions = self.TabSheet:NewTab("Your Auctions", "selfauctions", "gui/arrow_up", "See your own auctions")
 	self.PickUpAuction = self.TabSheet:NewTab("Pick up Auctions", "pickupauctionstab", "gui/arrow_up", "Pick up auctions here.")
 
-	self.Frame.btnClose = vgui.Create("DButton", self.Frame)
-	self.Frame.btnClose:SetFont("Marlett")
-	self.Frame.btnClose:SetText("r")
-	self.Frame.btnClose.DoClick = function(pnlPanel)
+	self.Frame.Close = vgui.Create("DButton", self.Frame)
+	self.Frame.Close:SetFont("Marlett")
+	self.Frame.Close:SetText("r")
+	self.Frame.Close.DoClick = function(pnlPanel)
 		GAMEMODE.AuctionMenu.Frame:Close()
 		GAMEMODE.AuctionMenu = nil
 	end
 
-	self.Frame.btnClose.Paint = function()
-		jdraw.QuickDrawPanel(clrGray, 0, 0, self.Frame.btnClose:GetWide() - 1, self.Frame.btnClose:GetTall() - 1)
+	self.Frame.Close.Paint = function()
+		jdraw.QuickDrawPanel(clrGray, 0, 0, self.Frame.Close:GetWide() - 1, self.Frame.Close:GetTall() - 1)
 	end
 	self.Frame:MakePopup()
 	self:PerformLayout()
@@ -28,7 +28,7 @@ end
 function PANEL:PerformLayout()
 	self.Frame:SetPos(self:GetPos())
 	self.Frame:SetSize(self:GetSize())
-	self.Frame.btnClose:SetPos(self.Frame:GetWide() - 5, 10)
+	self.Frame.Close:SetPos(self.Frame:GetWide() - 5, 10)
 
 	self.TabSheet:SetPos(5, 5)
 	self.TabSheet:SetSize(self.Frame:GetWide() - 10, self.Frame:GetTall() - 10)
@@ -37,8 +37,8 @@ vgui.Register("auctionmenu", PANEL, "Panel")
 
 concommand.Add("UD_OpenAuctionMenu", function(ply, command, args)
 	local npc = ply:GetEyeTrace().Entity
-	local tblNPCTable = NPCTable(npc:GetNWString("npc"))
-	if not IsValid(npc) or not tblNPCTable or not tblNPCTable.Auction then return end
+	local NPCTable = NPCTable(npc:GetNWString("npc"))
+	if not IsValid(npc) or not NPCTable or not NPCTable.Auction then return end
 	RunConsoleCommand("UD_RequestAuctionInfo")
 	GAMEMODE.AuctionMenu = GAMEMODE.AuctionMenu or vgui.Create("auctionmenu")
 	GAMEMODE.AuctionMenu:SetSize(520, 459)
